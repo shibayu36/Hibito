92a61825d70e53ada2669cbfae643e7a58c6f7e0
shibayu36 <shibayu36@gmail.com>
2025-06-11 08:50:19 +0900

    プロジェクト作成

---
+++ Hibito/HibitoApp.swift
@@ -0,0 +1,32 @@
+//
+//  HibitoApp.swift
+//  Hibito
+//
+//  Created by Yuki Shibazaki on 2025/06/11.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HibitoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Hibito/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Hibito
+//
+//  Created by Yuki Shibazaki on 2025/06/11.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
